8e0548985f7cf8fb6d21940104fcb726b48fe61a
yaswanthr233 <yaswanthr233@gmail.com>
2026-03-14 10:29:01 +0530

    Initial Commit

---
+++ 2400030678_FSAD_SKILL_02/src/main/java/com/klu/entity/Product.java
@@ -0,0 +1,69 @@
+package com.klu.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private int id;
+
+    private String name;
+    private String description;
+    private double price;
+    private int quantity;
+    public int getId() {
+		return id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	
+
+    public Product(){}
+
+    public Product(String name,String description,double price,int quantity){
+        this.name=name;
+        this.description=description;
+        this.price=price;
+        this.quantity=quantity;
+    }
+
+}
---
+++ 2400030678_FSAD_SKILL_02/src/main/java/com/klu/util/HibernateUtil.java
@@ -0,0 +1,13 @@
+package com.klu.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
